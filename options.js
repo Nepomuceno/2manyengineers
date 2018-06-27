d9ab1e39810bfbdc53f81f48ed08c5ee3538841f
Gabriel Nepomuceno <gabriel.bico@gmail.com>
2018-06-27 16:19:51 +0100

    correct

--- options.js
+++ options.js
@@ -3,6 +3,7 @@ let selectionValue = document.getElementById('charity');
 let percentageValue = document.getElementById('percentageChange');
 
 selectionValue.addEventListener('change', (src, ev) => {
+    console.log(percentageValue.value);
     chrome.storage.sync.set(
         {
             color: '#3aa757',
@@ -14,6 +15,7 @@ selectionValue.addEventListener('change', (src, ev) => {
 })
 
 percentageValue.addEventListener('change', (src, ev) => {
+    console.log(percentageValue.value);
     chrome.storage.sync.set(
         {
             color: '#3aa757',
@@ -23,3 +25,48 @@ percentageValue.addEventListener('change', (src, ev) => {
             console.info(ev);
         })
 })
+
+
+$(function() {
+    $('.project').each(function() {
+      var $projectBar = $(this).find('.bar');
+      var $projectPercent = $(this).find('.percent');
+      $projectBar.slider({
+        range: "min",
+        animate: true,
+        value: 0.5,
+        min: 0,
+        max: 1,
+        step: 0.01,
+        slide: function(event, ui) {
+          $projectPercent.val(ui.value * 100 + "%");
+        },
+        change: function(event, ui) {
+          var $projectRange = $(this).find('.ui-slider-range');
+          var percent = ui.value;
+          if (percent < 30) {
+            $projectPercent.css({
+              'color': 'red'
+            });
+            $projectRange.css({
+              'background': '#f20000'
+            });
+          } else if (percent > 31 && percent < 70) {
+            $projectPercent.css({
+              'color': 'gold'
+            });
+            $projectRange.css({
+              'background': 'gold'
+            });
+          } else if (percent > 70) {
+            $projectPercent.css({
+              'color': 'green'
+            });
+            $projectRange.css({
+              'background': 'green'
+            });
+          }
+        }
+      });
+    })
+  })
--- popup.js
+++ popup.js
@@ -1,41 +1,5 @@
-let savenow = document.getElementById('savenow');
-let charity = "Unicef";
-let percentage = 0.5;
 
-chrome.storage.sync.get('color', function (data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
-});
-
-chrome.storage.sync.get('percentage', function (data) {
-    console.info(data);
-    percentage = data.percentage;
-});
-
-chrome.storage.sync.get('charity', function (data) {
-    console.info(data);
-    charity = data.charity;
-});
-
-
-
-changeColor.onclick = function (element) {
-    let color = element.target.value;
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            { code: 'document.body.style.backgroundColor = "' + color + '";' });
-
-    });
-};
 const regex = /((?<=data-asin=\")(?<name>[a-zA-Z0-9\.\,]*))|(?<=data-asin-price=\")(?<price>[0-9\.\,]*)/gm;
-
-function GetLower(product, price) {
-    return [{
-        url: "",
-        difference: "30.00"
-    }];
-}
 
 
 chrome.runtime.onMessage.addListener(function (request, sender) {
@@ -48,24 +12,56 @@ chrome.runtime.onMessage.addListener(function (request, sender) {
 
         var prices = this.GetLower(content.groups.name, content.groups.price);
         if (prices.length > 0) {
-            message.innerText += `Save £${prices[0].difference} and donate £${prices[0].difference * percentage} to save a child`
-            if(savenow){
-                savenow.href = `${prices[0].url}`
-            }
+            let productNameEl = document.getElementById('product');
+            let charityLogo = document.getElementById('charityLogo');
+            let charityEl = document.getElementById('charity');
+            let savings = document.getElementById('savings');
+            let savingLink = document.getElementById('savingLink');
+            let result = document.getElementById('result');
+            console.log('Changing');
+            console.info(productNameEl);
+            productNameEl.innerHTML = prices[0].name;
+            charityLogo.setAttribute('src', 'images/wateraid.png')
+            charityEl.innerHTML = charity;
+            savings.innerHTML = `£${prices[0].difference}`;
+            savingLink.setAttribute('href', prices[0].url);
+            console.log(percentage);
+            result.innerHTML = `Water for children for ${(prices[0].difference * percentage) / 5} months`
         }
 
-        while ((m = regex.exec(request.source)) !== null) {
-            // This is necessary to avoid infinite loops with zero-width matches
-            if (m.index === regex.lastIndex) {
-                regex.lastIndex++;
-            }
-            // The result can be accessed through the `m`-variable.
-            m.forEach((match, groupIndex) => {
-                console.log(match);
-            });
-        }
+
     }
 });
+/*
+<img src="http://charityLogo/" id="charityLogo" />
+    <p id="charity">Unicef</p>
+    <p id="savings">£10.00</p>
+    <a href="http://www.amazing.com" id="savingLink"></a>
+    <p id="result">Saves 3 childrem</p>
+    */
+let charity = "Unicef";
+let percentage = 0.5;
+chrome.storage.sync.get('percentage', function (data) {
+    console.info(data);
+    percentage = data.percentage;
+});
+
+chrome.storage.sync.get('charity', function (data) {
+    console.info(data);
+    charity = data.charity;
+});
+
+function GetLower(product, price) {
+    return [{
+        name: "Adidas Originals Gazelle Trainers In Navy",
+        url: "https://cheapandcheerful.azurewebsites.net/products",
+        difference: "30.00",
+
+    }];
+}
+
+
+
 
 function onWindowLoad() {
 
