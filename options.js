e282db9d6e65107943256b5fb9ec57d86602a7c1
Gabriel Nepomuceno <gabriel.bico@gmail.com>
2018-06-27 16:28:28 +0100

    savings

--- options.js
+++ options.js
@@ -27,46 +27,55 @@ percentageValue.addEventListener('change', (src, ev) => {
 })
 
 
-$(function() {
-    $('.project').each(function() {
-      var $projectBar = $(this).find('.bar');
-      var $projectPercent = $(this).find('.percent');
-      $projectBar.slider({
-        range: "min",
-        animate: true,
-        value: 0.5,
-        min: 0,
-        max: 1,
-        step: 0.01,
-        slide: function(event, ui) {
-          $projectPercent.val(ui.value * 100 + "%");
-        },
-        change: function(event, ui) {
-          var $projectRange = $(this).find('.ui-slider-range');
-          var percent = ui.value;
-          if (percent < 30) {
-            $projectPercent.css({
-              'color': 'red'
-            });
-            $projectRange.css({
-              'background': '#f20000'
-            });
-          } else if (percent > 31 && percent < 70) {
-            $projectPercent.css({
-              'color': 'gold'
-            });
-            $projectRange.css({
-              'background': 'gold'
-            });
-          } else if (percent > 70) {
-            $projectPercent.css({
-              'color': 'green'
-            });
-            $projectRange.css({
-              'background': 'green'
-            });
-          }
-        }
-      });
+$(function () {
+    $('.project').each(function () {
+        var $projectBar = $(this).find('.bar');
+        var $projectPercent = $(this).find('.percent');
+        $projectBar.slider({
+            range: "min",
+            animate: true,
+            value: 0.5,
+            min: 0,
+            max: 1,
+            step: 0.01,
+            slide: function (event, ui) {
+                $projectPercent.val(ui.value * 100 + "%");
+            },
+            change: function (event, ui) {
+                console.log(percentageValue.value);
+                chrome.storage.sync.set(
+                    {
+                        color: '#3aa757',
+                        percentage: ui.value,
+                        charity: selectionValue.value,
+                    }, function () {
+                        console.info(event);
+                    });
+                var $projectRange = $(this).find('.ui-slider-range');
+                var percent = ui.value;
+                if (percent < .3) {
+                    $projectPercent.css({
+                        'color': 'red'
+                    });
+                    $projectRange.css({
+                        'background': '#f20000'
+                    });
+                } else if (percent > .31 && percent < .70) {
+                    $projectPercent.css({
+                        'color': 'gold'
+                    });
+                    $projectRange.css({
+                        'background': 'gold'
+                    });
+                } else if (percent > .70) {
+                    $projectPercent.css({
+                        'color': 'green'
+                    });
+                    $projectRange.css({
+                        'background': 'green'
+                    });
+                }
+            }
+        });
     })
-  })
+})
